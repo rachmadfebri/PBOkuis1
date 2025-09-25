23e3c23728fc5b736186013e4fb44d145fd07bbc
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2025-09-25 19:59:54 +0700

    instruktur_peserta

--- Instruktur.java
+++ Instruktur.java
@@ -13,4 +13,16 @@ public class Instruktur extends Pengguna {
     public void setKeahlian(String keahlian) { 
         this.keahlian = keahlian; 
     }
+    public Kursus buatKursus(String namaKursus, String deskripsi, double harga) {
+        return new Kursus(1, namaKursus, deskripsi, harga);
+    }
+
+    public void editKonten(Konten k) {
+        System.out.println("Instruktur " + getNama() + " mengedit konten " + k.getJudul());
+    }
+
+    public List<Konten> lihatKursus() {
+        System.out.println("Instruktur " + getNama() + " melihat daftar kursus yang dibuat.");
+        return null;
+    }
 }
--- Peserta.java
+++ Peserta.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.Date;
 
 public class Peserta extends Pengguna{
 
@@ -7,5 +8,18 @@ public class Peserta extends Pengguna{
     }
      public void daftarKursus(Kursus k) {
         System.out.println(getNama() + " mendaftar ke kursus " + k.getNamaKursus());
+    }
+    public List<Kursus> lihatKursusTerdaftar() {
+        System.out.println("menampilkan daftar kursus yang diikuti " + getNama());
+        return null;
+    }
+    public Pembayaran bayarKursus(Kursus k) {
+        System.out.println(getNama() + " membayar kursus " + k.getNamaKursus() + " sebesar " + k.getHarga());
+        return new Pembayaran(1, new Date(), k.getHarga()); 
+    }
+    public double ambilKuis(Kuis kuis, int[] jawaban) {
+        double skor = kuis.evaluasiJawaban(jawaban);
+        System.out.println(getNama() + " mendapatkan skor: " + skor + "% pada kuis " + kuis.getJudul());
+        return skor;
     }
 }
