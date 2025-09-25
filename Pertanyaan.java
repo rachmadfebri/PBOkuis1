125a9b6d45a7e7d2b33af0e9532ffa5a1795e482
martaaa-16 <martaprama@gmail.com>
2025-09-25 18:25:11 +0700

    add class Pertanyaan

---
+++ Pertanyaan.java
@@ -0,0 +1,33 @@
+public class Pertanyaan {
+    private int idPertanyaan;
+    private String teks;
+    private PilihanJawaban[] pilihan;
+    private int jumlahPilihan;
+
+    public Pertanyaan(int idPertanyaan, String teks, int kapasitasPilihan) {
+        this.idPertanyaan = idPertanyaan;
+        this.teks = teks;
+        this.pilihan = new PilihanJawaban[kapasitasPilihan];
+        this.jumlahPilihan = 0;
+    }
+
+    public void tambahPilihan(PilihanJawaban p) {
+        if (jumlahPilihan < pilihan.length) {
+            pilihan[jumlahPilihan] = p;
+            jumlahPilihan++;
+        } else {
+            System.out.println("Pilihan sudah penuh!");
+        }
+    }
+
+    public void tampilkanPertanyaan() {
+        System.out.println("Q" + idPertanyaan + ": " + teks);
+        for (int i = 0; i < jumlahPilihan; i++) {
+            System.out.println("   " + (i + 1) + ". " + pilihan[i].getTeksPilihan());
+        }
+    }
+
+    public PilihanJawaban[] getPilihan() {
+        return pilihan;
+    }
+}
